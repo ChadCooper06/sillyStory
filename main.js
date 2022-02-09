5e33f958b1127ed6b89df63770c4e14865b24a1d
ChadCooper06 <98066630+ChadCooper06@users.noreply.github.com>
2022-02-09 13:33:24 -0500

    punctuation correction/changed var to const

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-var insertX = ("Willy the Goblin", "Big Daddy", "Father Christmas");
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 
-var insertY = ("the soup kitchen", "Disneyland", "the White House");
+const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 
-var insertZ = ("spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away");
+const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
 
 randomize.addEventListener('click', result);
@@ -39,8 +39,8 @@ function result() {
   if(document.getElementById("uk").checked) {
     const weight = Math.round(300/ 2.205) + 'stone';
     const temperature =  Math.round((94-32) * 5/9) + 'centigrade';
-    newStory = newStory (text.replace ('94 Fahrenheit', temperature);
-    newStory = newStory (text.replace ('300 pounds', weight);
+    newStory = newStory (text.replace ('94 Fahrenheit', temperature));
+    newStory = newStory (text.replace ('300 pounds', weight));
   }
 
   story.textContent = "newStory";
